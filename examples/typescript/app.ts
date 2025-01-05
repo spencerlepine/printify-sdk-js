eb67938d88c73519bb624e7c6f01ee7988259551
Spencer Lepine <60903378+spencerlepine@users.noreply.github.com>
2025-01-05 12:05:51 +0000

    update examples

--- examples/typescript/app.ts
+++ examples/typescript/app.ts
@@ -7,8 +7,16 @@ const printify = new Printify({
   enableLogging: true,
 });
 
+// v1 endpoint
 (async () => {
   const result: ListWebhooksResponse = await printify.webhooks.list();
   const webhook: Webhook = result[0];
   console.log(webhook); // { "topic": "order:created", "url": "https://example.com/webhooks/order/created", "shop_id": "1", "id": "5cb87a8cd490a2ccb256cec4" }
+})();
+
+// TODO v1.3.0
+// v2 endpoint
+(async () => {
+  const shippingInfo = await printify.v2.catalog.getShippingListInfo('1', '2');
+  console.log(shippingInfo);
 })();
